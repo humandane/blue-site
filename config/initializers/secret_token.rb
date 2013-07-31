d6ac886542c49adac9a739c0c443a6d4e0fdbe49
Dane Guempel <Dane.Guempel@cerner.com>
2013-07-31 10:57:33 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlueSite::Application.config.secret_key_base = 'a88a858426057fc8b6732d00c0eb5d7f1118cdd7aec8d99e5b25564db8df1a083b4e32d458f581e97f8c70fcf7b8d09d8dfaf8a4b146e29c4e8f0f5a63fc4162'
